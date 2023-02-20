2e73595093d8a59fb6d0c460dbe26d962da699bd
zinakalu <zina.kalu@gmail.com>
2023-02-20 13:54:16 -0500

    created arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random()*1)+1;
+
+let num3 = 12;
+let num4 = 3;
+
+let mod = num3 / num4;
+console.log(mod)
+
+let max = Math.max( 5, 10, 15, 20);
